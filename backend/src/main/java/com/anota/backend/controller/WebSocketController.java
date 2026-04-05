45378810f0ad7fca832f9a159f467075ca7ae920
FelipeLeiteConrado <flc9019@gmail.com>
2026-04-05 16:36:33 -0300

    feat: websocket config and real-time broadcast

---
+++ backend/src/main/java/com/anota/backend/controller/WebSocketController.java
@@ -0,0 +1,21 @@
+package com.anota.backend.controller;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public WebSocketController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/doc/{slug}/edit")
+    public void handleEdit(@DestinationVariable String slug, String delta) {
+        simpMessagingTemplate.convertAndSend("/topic/doc/" + slug, delta);
+    }
+}
